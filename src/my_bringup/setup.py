7a0c2767e71ce0e59711d491ea62654d722bbe41
jinshim-commits <tlathwls0518@gmail.com>
2025-12-06 15:22:59 +0900

    런치파일 설치 경로 추가
    저 패키지 안에서 UI실행하는 런치 파일 생성해놔서 경로 추가했습니다

--- src/my_bringup/setup.py
+++ src/my_bringup/setup.py
@@ -10,6 +10,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        #런치 파일 설치 경로 1줄 추가(심소진)
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
