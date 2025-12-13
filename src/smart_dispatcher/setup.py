3638f3acba4b0399a365d67a5819c06d7723be6d
jinshim-commits <tlathwls0518@gmail.com>
2025-12-13 15:46:27 +0900

    Refactor setup.py to use find_packages()

--- src/smart_dispatcher/setup.py
+++ src/smart_dispatcher/setup.py
@@ -1,22 +1,21 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import os
-from glob import glob  # <--- [1. 맨 위에 이 줄 추가하세요!]
+from glob import glob
 
 package_name = 'smart_dispatcher'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(),   # <-- 반드시 이렇게 해야 함
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        # ---------------------------------------------------------
-        # [2. 아래 줄을 복사해서 여기에 끼워 넣으세요!]
-        # launch 폴더 안에 있는 모든 .launch.py 파일을 설치하라는 뜻입니다.
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        # ---------------------------------------------------------
+
+        # launch 파일 설치
+        (os.path.join('share', package_name, 'launch'),
+         glob('launch/*.launch.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -27,7 +26,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'dispatcher = smart_dispatcher.dispatcher_node:main',
+            'dispatcher_node = smart_dispatcher.dispatcher_node:main',
         ],
     },
 )
