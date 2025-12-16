92abc8238383acaa9ffcf75c258b4168204f4d83
raybeak <ray.beak@outlook.com>
2025-12-16 18:52:30 +0900

    added frontend prototype -dont expect too much on it

--- src/frontend/frontend.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-#타이틀로 의사 소견서
-st.title("의사 소견서")
-st.header(f"성명: {name}")
-st.subheader(f"나이: {age}")
-st.write("this will be limo_dashbord fontend")
-st.caption("주식회사 응기잇")
